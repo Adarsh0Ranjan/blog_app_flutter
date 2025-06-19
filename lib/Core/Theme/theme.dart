f97e704153140783ee985dcc004f205bd898e059
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-06-19 20:55:08 +0530

    theme dart updated

--- lib/Core/Theme/theme.dart
+++ lib/Core/Theme/theme.dart
@@ -2,7 +2,16 @@ import 'package:blog_app_flutter/Core/Theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final  darkThemeMode = ThemeData.dark().copyWith(
+  static  _border({Color color = AppPallete.borderColor}) => OutlineInputBorder(
+    borderSide: BorderSide(color: color, width: 3),
+    borderRadius: BorderRadius.circular(10),
+  );
+  static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(color: AppPallete.gradient2)
+    ),
   );
 }
