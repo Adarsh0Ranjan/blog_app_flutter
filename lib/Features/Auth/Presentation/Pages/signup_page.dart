c3e1dcbd178d7e7952a386e32929224812fd0a78
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-06-19 20:55:33 +0530

    auth text form filed added

--- lib/Features/Auth/Presentation/Pages/signup_page.dart
+++ lib/Features/Auth/Presentation/Pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app_flutter/Features/Auth/Presentation/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
@@ -10,6 +11,25 @@ class SignupPage extends StatefulWidget {
 class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Sign Up.',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            AuthField(hintText: 'Name'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Email'),
+            const SizedBox(height: 15),
+            AuthField(hintText: 'Password'),
+          ],
+        ),
+      ),
+    );
   }
 }
