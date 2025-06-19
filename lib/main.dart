9191f28c66e5c8537aa263a8dd582ac62f916993
Adarsh Ranjan <adarsh.ranjan@roro.io>
2025-06-19 20:03:46 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:blog_app_flutter/Core/Theme/theme.dart';
+import 'package:blog_app_flutter/Features/Auth/Presentation/Pages/signup_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: AppTheme.darkThemeMode,
+      home: SignupPage(),
+    );
+  }
+}
+
+
